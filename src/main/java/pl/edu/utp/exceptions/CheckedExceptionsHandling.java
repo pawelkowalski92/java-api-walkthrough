7d0288e3a9483e28237bfc6e75724b3f9d3c4995
Paweł Kowalski <pawel.kowalski.2@asseco.pl>
2021-04-26 19:17:30 +0200

    Checked exceptions with Java IO

--- src/main/java/pl/edu/utp/exceptions/CheckedExceptionsHandling.java
+++ src/main/java/pl/edu/utp/exceptions/CheckedExceptionsHandling.java
@@ -7,11 +7,33 @@ import java.nio.file.Path;
 
 public class CheckedExceptionsHandling {
 
+    // overly-verbose way to read a file ;)
     public static void main(String[] args) {
         Path fileToRead = Path.of(args[0]);
+        readFileOverlyVerbose(fileToRead);
+        readFileInTryWithResources(fileToRead);
+        readFileUsingNIO(fileToRead);
+    }
+
+    static String invertCase(String line) throws Exception {
+        char[] chars = line.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+//            if (i > 50) {
+//                // we're simulating an error in the code
+//                throw new Exception("error in inversion");
+//            }
+            chars[i] = Character.isUpperCase(c)
+                    ? Character.toLowerCase(c)
+                    : Character.toUpperCase(c);
+        }
+        return new String(chars);
+    }
+
+    static void readFileOverlyVerbose(Path pathToFile) {
         BufferedReader reader = null;
         try {
-            reader = Files.newBufferedReader(fileToRead);
+            reader = Files.newBufferedReader(pathToFile);
             String line = reader.readLine();
             while (line != null) {
                 String invertedCaseLine = invertCase(line);
@@ -19,32 +41,51 @@ public class CheckedExceptionsHandling {
                 line = reader.readLine();
             }
         } catch (IOException ex) {
-            System.err.println("Cannot read file: " + fileToRead);
+            System.err.println("Cannot read file: " + pathToFile);
+            ex.printStackTrace();
+        } catch (Exception ex) {
+            System.err.println("Cannot invert case");
             ex.printStackTrace();
         } finally {
             if (reader != null) {
                 try {
                     reader.close();
                 } catch (IOException ex) {
-                    System.err.println("Cannot close reader for file: " + fileToRead);
+                    System.err.println("Cannot close reader for file: " + pathToFile);
                 }
             }
         }
     }
 
-    static String invertCase(String line) {
-        char[] chars = line.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            char c = chars[i];
-            if (i > 10) {
-                // we're simulating an error in the code
-                throw new IllegalStateException("ops");
+    static void readFileInTryWithResources(Path pathToFile) {
+        try (BufferedReader reader = Files.newBufferedReader(pathToFile)) {
+            String line = reader.readLine();
+            while (line != null) {
+                String invertedCaseLine = invertCase(line);
+                System.out.println(invertedCaseLine);
+                line = reader.readLine();
             }
-            chars[i] = Character.isUpperCase(c)
-                    ? Character.toLowerCase(c)
-                    : Character.toUpperCase(c);
+        } catch (IOException ex) {
+            System.err.println("Cannot read file: " + pathToFile);
+            ex.printStackTrace();
+        } catch (Exception ex) {
+            System.err.println("Cannot invert case");
+            ex.printStackTrace();
         }
-        return new String(chars);
+    }
+
+    static void readFileUsingNIO(Path pathToFile) {
+        try {
+            String line = Files.readString(pathToFile);
+            String invertedCaseLine = invertCase(line);
+            System.out.println(invertedCaseLine);
+        } catch (IOException ex) {
+            System.err.println("Cannot read file: " + pathToFile);
+            ex.printStackTrace();
+        } catch (Exception ex) {
+            System.err.println("Cannot invert case");
+            ex.printStackTrace();
+        }
     }
 
 }
