9d914a34aad7ff5e60769e132cef16685e7ac2a1
Paweł Kowalski <pawel.kowalski.2@asseco.pl>
2021-04-30 08:42:27 +0200

    Basic queue example

--- src/main/java/pl/edu/utp/collections/QueueExample.java
+++ src/main/java/pl/edu/utp/collections/QueueExample.java
@@ -5,6 +5,7 @@ import pl.edu.utp.collections.models.Book;
 import java.util.ArrayDeque;
 import java.util.NoSuchElementException;
 import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class QueueExample {
 
@@ -27,20 +28,18 @@ public class QueueExample {
         }
 
         // add element
-        try {
-            boolean didAdd = bookStack.add(new Book("HTML for architects", 5));
-            boolean didOffer = bookStack.offer(new Book("CSS for architects", 5));
-            System.out.println("Add? " + didAdd);
-            System.out.println("Offer? " + didOffer);
-        } catch (IllegalStateException ex) {
-            System.out.println("bookStack.add() has no capacity to add new elements");
-        }
+        addToQueue(bookStack);
 
         printAllBooks(bookStack);
         System.out.println(bookStack);
 
         printAllBooksNoIterator(bookStack);
         System.out.println(bookStack);
+
+        // queue with limited size
+        Queue<Book> sizeLimited = new ArrayBlockingQueue<>(1);
+        addToQueue(sizeLimited);
+        addToQueue(sizeLimited);
     }
 
     public static void printAllBooks(Queue<Book> bookStack)  {
@@ -52,6 +51,18 @@ public class QueueExample {
     public static void printAllBooksNoIterator(Queue<Book> bookStack) {
         for (Book book = bookStack.poll(); book != null; book = bookStack.poll()) {
             System.out.println(book);
+        }
+    }
+
+    public static void addToQueue(Queue<Book> bookStack) {
+        // add element
+        try {
+            boolean didAdd = bookStack.add(new Book("HTML for architects", 5));
+            boolean didOffer = bookStack.offer(new Book("CSS for architects", 5));
+            System.out.println("Add? " + didAdd);
+            System.out.println("Offer? " + didOffer);
+        } catch (IllegalStateException ex) {
+            System.out.println("bookStack.add() has no capacity to add new elements");
         }
     }
 
