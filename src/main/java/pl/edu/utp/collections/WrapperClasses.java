2435f30d16892ab305bb276cec407e6e71f0bdaf
Paweł Kowalski <pawel.kowalski.2@asseco.pl>
2021-04-30 08:43:11 +0200

    Wrapper classes applicability in overloaded methods

--- src/main/java/pl/edu/utp/collections/WrapperClasses.java
+++ src/main/java/pl/edu/utp/collections/WrapperClasses.java
@@ -1,5 +1,8 @@
 package pl.edu.utp.collections;
 
+import pl.edu.utp.collections.models.Book;
+import pl.edu.utp.collections.models.FantasyBook;
+
 import java.util.*;
 
 public class WrapperClasses {
@@ -31,6 +34,49 @@ public class WrapperClasses {
 
         int c = ints.getLast(); // unboxing: int c = ints.getLast().intValue();
         System.out.println(c);
+
+        int result = addInts(a, c); // autoboxing: addInts(Integer.valueOf(a), Integer.valueOf(c));
+        System.out.println("Sum: " + result);
+
+        // a small recap on method overloading
+
+        int res1 = addInts(10, 20); // ?
+        System.out.println(res1);
+
+        int res2 = addInts(Integer.valueOf(1), Integer.valueOf(2)); // ?
+        System.out.println(res2);
+
+        // method used in overloading depends on the reference the object is assigned to
+        Book book = new Book("a", 1);
+        Book fantasyBook = new FantasyBook("b", 2);
+
+        System.out.println("Book description: " + getDescription(book));
+        System.out.println("Fantasy book description: " + getDescription(fantasyBook));
+    }
+
+    public static int addInts(Integer a, Integer b) {
+        System.out.println("objects");
+        return a + b;
+    }
+
+    public static int addInts(int a, int b) {
+        System.out.println("primitives");
+        return a + b;
+    }
+
+    public static String getDescription(Object object) {
+        System.out.println("from object");
+        return object.toString();
+    }
+
+    public static String getDescription(Book book) {
+        System.out.println("from book");
+        return book.toString();
+    }
+
+    public static String getDescription(FantasyBook fantasyBook) {
+        System.out.println("from fantasy book");
+        return fantasyBook.toString();
     }
 
 }
